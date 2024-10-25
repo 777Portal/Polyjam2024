a1d663780a2e9abe96f18ddb97e4e432e98572c1
exon <throwawayact69xd@gmail.com>
2024-10-25 12:43:57 -0400

    Fixed landing page not rendering images and nav button not working

--- public/landing.js
+++ public/landing.js
@@ -2,6 +2,6 @@ document.getElementById("bottomBtn").onclick = function () {
     location.href = "https://discord.com/oauth2/authorize?client_id=1220892940621053967&response_type=code&redirect_uri=https%3A%2F%2Fwww.polyjam.win%2Fauthenticate&scope=identify";
 };
 
-document.getElementById("bottomBtn").onclick = function () {
+document.getElementById("navBtn").onclick = function () {
     location.href = "https://discord.com/oauth2/authorize?client_id=1220892940621053967&response_type=code&redirect_uri=https%3A%2F%2Fwww.polyjam.win%2Fauthenticate&scope=identify";
 };
