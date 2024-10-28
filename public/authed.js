9881d7273d25b2375f481f4d91794eabd043a6e4
exon <throwawayact69xd@gmail.com>
2024-10-28 15:04:39 -0400

    Updated authed.js so if user isn't logged in there are fallbacks.

--- public/authed.js
+++ public/authed.js
@@ -1,26 +1,63 @@
 // this is kinda like just a utill stuff that i need in a lot of pages, if not most.
+function roundNumber(num){
+  return Math.round(num * 100) / 100
+}
+
+// add comma to numbers we get to make it more readable
+function addCommaToNumber(x) {
+  x = roundNumber(x)
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// get timestamp
+function getTimeStamp() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const amOrPm = hours >= 12 ? 'PM' : 'AM';
+  const formattedHours = (hours % 12 || 12).toString();
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  return `${formattedHours}:${formattedMinutes} ${amOrPm}`;
+}
+
+function toggleVis(elementId){
+  var x = document.getElementById(elementId);
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  
+  x.style.display = "none";
+}
+
 document.onload = innit()
 
 async function innit(){
   const response = await fetch("https://www.polyjam.win/me");
   var profile = await response.json();
 
+  profileImgs = document.getElementsByClassName('myProfileImg')
+  username = document.getElementById('username')
+
   // update to how /me endpoint works to include dev
   profile = profile.profile
-  if (!profile) return;
+  if (!profile) {
+    eggsBalance = document.getElementById('balTopBar');
+    toggleVis(eggsBalance);
+    profileImgs.src = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥚</text></svg>";
+    username.innerText += ` Guest ]`;
+    return;
+  }
 
   let id = profile.id
   let avatar = profile.avatar
   let name = profile.global_name
 
-  profileImgs = document.getElementsByClassName('myProfileImg')
-  
   // support multiple... why does it support multiple?
   for (var i = 0; i < profileImgs.length; i++) {
     profileImgs[i].src = `https://cdn.discordapp.com/avatars/${id}/${avatar}`
   }
 
-  username = document.getElementById('username')
   username.innerText += ` ${name} ]`
 
   connect();
@@ -59,7 +96,7 @@ function connect(){
 }
 
 window.onfocus = function() {
-  document.title = "Average EC";
+  document.title = "AEC";
   focused = true
 };
 
@@ -68,39 +105,3 @@ window.onblur = function() {
   document.title = `AEC - (${eggAmount})`;
   focused = false
 };
-
-function roundNumber(num){
-    return Math.round(num * 100) / 100
-}
-
-// add comma to numbers we get to make it more readable
-function addCommaToNumber(x) {
-    x = roundNumber(x)
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
-
-// get timestamp
-function getTimeStamp() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const amOrPm = hours >= 12 ? 'PM' : 'AM';
-    const formattedHours = (hours % 12 || 12).toString();
-    const formattedMinutes = minutes.toString().padStart(2, '0');
-    return `${formattedHours}:${formattedMinutes} ${amOrPm}`;
-}
-
-// grabbed from my website :D
-function toggleVis(elementId)
-{
-  var x = document.getElementById(elementId);
-
-  if (x.style.display === "none") 
-  {
-    x.style.display = "block";
-  }
-    else 
-  {
-    x.style.display = "none";
-  }
-}
