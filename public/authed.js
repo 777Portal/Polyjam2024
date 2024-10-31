31e588241059306e51c03b091852a7234e938d13
exon <throwawayact69xd@gmail.com>
2024-10-31 10:26:41 -0400

    Updated authed.js to hide sidebar elements if not logged in.

--- public/authed.js
+++ public/authed.js
@@ -47,6 +47,13 @@ async function innit(){
 
     profileImgs.src = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥚</text></svg>";
     username.innerText += ` Guest ]`;
+
+    shopSidebar = document.getElementById('shop');
+    shopSidebar.style.display = "none";
+
+    profileSidebar = document.getElementById('profile');
+    profileSidebar.style.display = "none";
+
     return;
   }
 
